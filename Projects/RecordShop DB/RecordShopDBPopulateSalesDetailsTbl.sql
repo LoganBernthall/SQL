45d023cf00fde57f153c06eef416993323af646e
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2024-02-04 11:49:36 +0000

    -

---
+++ Projects/RecordShop DB/RecordShopDBPopulateSalesDetailsTbl.sql
@@ -0,0 +1,5 @@
+INSERT INTO SalesDetails (QuantitySold, PricePerUnit, SalesID, ProductID) VALUES (2, 12, 1, 1);
+INSERT INTO SalesDetails (QuantitySold, PricePerUnit, SalesID, ProductID) VALUES (7, 28, 2, 4);
+INSERT INTO SalesDetails (QuantitySold, PricePerUnit, SalesID, ProductID) VALUES (3, 27, 3, 6);
+INSERT INTO SalesDetails (QuantitySold, PricePerUnit, SalesID, ProductID) VALUES (5, 12, 4, 2);
+INSERT INTO SalesDetails (QuantitySold, PricePerUnit, SalesID, ProductID) VALUES (1, 25, 5, 3);
