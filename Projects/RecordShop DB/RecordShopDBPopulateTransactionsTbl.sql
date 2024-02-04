90e7d79466737988890a7196c77e3739244a3aa7
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2024-02-04 11:57:33 +0000

    -

---
+++ Projects/RecordShop DB/RecordShopDBPopulateTransactionsTbl.sql
@@ -0,0 +1,5 @@
+INSERT INTO Transactions (TransactionID, TransactionType, ProductID) VALUES (1,'Card Payment',1);
+INSERT INTO Transactions (TransactionID, TransactionType, ProductID) VALUES (2,'Cash Payment',4);
+INSERT INTO Transactions (TransactionID, TransactionType, ProductID) VALUES (3,'Contactless Payment',6);
+INSERT INTO Transactions (TransactionID, TransactionType, ProductID) VALUES (4,'Cheque',2);
+INSERT INTO Transactions (TransactionID, TransactionType, ProductID) VALUES (5,'Card Payment',3);
